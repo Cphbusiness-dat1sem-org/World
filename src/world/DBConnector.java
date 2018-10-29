1e0e23d7992ad7e01b9b0741f7ec8ac147e2d8e2
EFIF\roda <roda@cphbusiness.dk>
2018-10-29 09:58:23 +0100

    Fix DBConnector for MySQL80

--- src/world/DBConnector.java
+++ src/world/DBConnector.java
@@ -2,6 +2,7 @@ package world;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -12,25 +13,30 @@ public class DBConnector {
     private Connection connection = null;
 	
     //Constants
-    private static final String IP	 = "localhost"; //142.93.109.235";
+    private static final String IP	 = "localhost";
     private static final String PORT     = "3307";
     public static final String DATABASE  = "world";
     private static final String USERNAME = "BigBoss"; 
     private static final String PASSWORD = "1234";	     	
 	
-    public DBConnector() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
-        Properties props = new Properties();
-        props.put("user", USERNAME);
-        props.put("password", PASSWORD);
-        props.put("allowMultiQueries", true);
-        props.put("useUnicode", true);
-        props.put("useJDBCCompliantTimezoneShift", true);
-        props.put("useLegacyDatetimeCode", false);
-        props.put("serverTimezone", "CET");
-        this.connection = DriverManager.getConnection(url, props);
-    }
+    public DBConnector() throws SQLException {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+                String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+                Properties props = new Properties();
+                props.put("user", USERNAME);
+                props.put("password", PASSWORD);
+                props.put("allowMultiQueries", true);
+                props.put("useUnicode", true);
+                props.put("useJDBCCompliantTimezoneShift", true);
+                props.put("useLegacyDatetimeCode", false);
+                props.put("serverTimezone", "CET");
+                this.connection = DriverManager.getConnection(url, props);
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+                ex.printStackTrace();
+                throw new SQLException("Class not found! (com.mysql.cj.jdbc.Driver)");
+            }
+	}
 
     public Connection getConnection() {
         return this.connection;
